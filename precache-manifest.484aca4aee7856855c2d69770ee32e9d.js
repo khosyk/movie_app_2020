726e106cc7f26bb0a968a4b7323b45658a956aab
hosy12@gmail.com <hosy12@gmail.com>
2020-10-23 14:50:54 +0900

    Updates

---
+++ precache-manifest.484aca4aee7856855c2d69770ee32e9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da49e2856939887223eea67ad030f36a",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "a200843b1437d2b17521",
+    "url": "/movie_app_2020/static/css/main.1a6dc005.chunk.css"
+  },
+  {
+    "revision": "98d9dc5dcbaedd216327",
+    "url": "/movie_app_2020/static/js/2.26ca2348.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/movie_app_2020/static/js/2.26ca2348.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a200843b1437d2b17521",
+    "url": "/movie_app_2020/static/js/main.71a1a0fd.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
